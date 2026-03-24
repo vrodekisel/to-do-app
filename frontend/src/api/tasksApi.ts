3d3a81550fd75680a1336a52b8074df47733e36d
vrodekisel <zabelloeug@gmail.com>
2026-03-24 18:31:32 +0300

    fix: clean api and add localization keys

--- frontend/src/api/tasksApi.ts
+++ frontend/src/api/tasksApi.ts
@@ -1,39 +1,11 @@
-import type { Task } from '../types/task';
+import type { Task } from "../types/task";
 
-const BASE_URL = 'http://localhost:5274/api/tasks';
-
-export async function getTasks(): Promise<Task[]> {
-    const response = await fetch(BASE_URL);
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch tasks');
-    }
-
-    const tasks: Task[] = await response.json();
-    return tasks;
-}
+const BASE_URL = "http://localhost:5274/api/tasks";
 
 export type CreateTaskData = {
     title: string;
     description: string;
 };
-
-export async function createTask(taskData: CreateTaskData): Promise<Task> {
-    const response = await fetch(BASE_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json',
-        },
-        body: JSON.stringify(taskData),
-    });
-
-    if (!response.ok) {
-        throw new Error('Failed to create task');
-    }
-
-    const createdTask: Task = await response.json();
-    return createdTask;
-}
 
 export type UpdateTaskData = {
     title: string;
@@ -41,26 +13,52 @@ export type UpdateTaskData = {
     isCompleted: boolean;
 };
 
-export async function updateTask(id : number, taskData: UpdateTaskData): Promise<void> {
-    const response = await fetch(`${BASE_URL}/${id}`, {
-        method: 'PUT',
+export async function getTasks(): Promise<Task[]> {
+    const response = await fetch(BASE_URL);
+
+    if (!response.ok) {
+        throw new Error("errors.failedToFetchTasks");
+    }
+
+    return response.json();
+}
+
+export async function createTask(taskData: CreateTaskData): Promise<Task> {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
         headers: {
-            'Content-Type' : 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(taskData),
     });
 
     if (!response.ok) {
-        throw new Error('Failed to update');
+        throw new Error("errors.failedToCreateTask");
+    }
+
+    return response.json();
+}
+
+export async function updateTask(id: number, taskData: UpdateTaskData): Promise<void> {
+    const response = await fetch(`${BASE_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(taskData),
+    });
+
+    if (!response.ok) {
+        throw new Error("errors.failedToUpdateTask");
     }
 }
 
 export async function deleteTask(id: number): Promise<void> {
     const response = await fetch(`${BASE_URL}/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
     });
 
     if (!response.ok) {
-        throw new Error('Failed to delete task');
+        throw new Error("errors.failedToDeleteTask");
     }
 }
